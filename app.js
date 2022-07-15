9a52dfabb3fc70d25c8c85e33b4fa2ac9ba356ff
kyleconyers <conyersky@gmail.com>
2022-07-14 21:31:33 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+// document.getElementById("demo").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+}
+
+function userRock() {
+    document.getElementById("rock").innerHTML = "clicked";
+}
+
+
+
+console.log("working");
